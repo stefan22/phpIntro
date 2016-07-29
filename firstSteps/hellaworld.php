c8341492ea39db2ef05598ce2fc463f841fab21d
stefan22 <stefan.emailacct@gmail.com>
2016-07-30 00:07:16 +0100

    first steps

---
+++ firstSteps/hellaworld.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Yellow World</title>
+		<meta name="description" content="basic html file">
+  		<meta name="author" content="basic html file">
+  		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<main>
+			<h1>
+				<?php echo "Hello World";
+
+				?>
+			</h1>
+
+			<p>if you view the code there's no php in it</p>
+
+
+
+		</main>	
+	</body>
+</html>
