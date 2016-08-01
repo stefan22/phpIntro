73e0bf2d698c9fd4beace895be07c4ae7298089f
stefan22 <stefan.emailacct@gmail.com>
2016-08-01 20:13:01 +0100

    associative arrays

---
+++ dataTypes/associate-arrays.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<title>sandbox</title>
+	</head>
+	<body>
+		
+
+	
+
+		<?php $assoc = array("first_name" => "Stefano", "last_name" => "Lissia"); ?>
+
+				<?php echo $assoc["first_name"]; ?>
+
+				<?php echo $assoc["first_name"]; ?> <br /><br />
+
+				<?php echo $assoc["last_name"]; ?>  <br /> <br />
+
+				<?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?>
+
+				<?php $assoc["first_name"] = "Michael";
+
+			
+		
+
+		
+
+
+				
+	</body>
+</html>
