d9b885caf4d53900333d6419a70126acf873c538
Stefano Lissia <stefan22@users.noreply.github.com>
2016-09-05 13:29:00 +0100

    Update floating.php

--- 02. dataTypes/floating.php
+++ 02. dataTypes/floating.php
@@ -2,53 +2,46 @@
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
-	<head>
-		<title>sandbox</title>
-	</head>
-	<body>
-		<?php
+<head>
+	<title>sandbox</title>
+</head>
+<body>
+	<?php
 			
-			echo $float = 3.14 . "<br/><br />";
+	echo $float = 3.14 . "<br/><br />";
 
-			echo $float + 7 . 	"<br/><br />";
+	echo $float + 7 . 	"<br/><br />";
 
-			echo 4 / 3	.	"<br/><br />";
+	echo 4 / 3	.	"<br/><br />";
 		
-			echo "<p>can't divide by zero</p>";
+	echo "<p>can't divide by zero</p>";
 	
-		?>
+	?>
 
 
-		<br />
+	<br />
 
-			Round: <?php echo round($float,1); ?> <br/><br/>
+	Round: <?php echo round($float,1); ?> <br/><br/>
 
-			Ceiling <?php echo ceil($float); ?> <br/><br/>
+	Ceiling <?php echo ceil($float); ?> <br/><br/>
 
-			Floor 	<?php echo floor($float); ?> <br/><br/>
+	Floor 	<?php echo floor($float); ?> <br/><br/>
 
-		<br/>
-
-
-			<?php $integer = 3; ?>
-			<?php $float = 3.14; ?>
-
-			<?php echo "Is {$integer} integer? " .  is_int($integer); ?> <br/><br/>
-
-			<?php echo "Is {$float} integer? "  .  is_int($float); ?> <br/><br/>
+	<br/>
 
 
-			<?php echo "Is {$integer} float? " .  is_float($integer); ?> <br/><br/>
+	<?php $integer = 3; ?>
+	<?php $float = 3.14; ?>
 
-			<?php echo "Is {$float} float? "  .  is_float($float); ?> <br/><br/>
+	<?php echo "Is {$integer} integer? " .  is_int($integer); ?> <br/><br/>
 
-
-
-
-
+	<?php echo "Is {$float} integer? "  .  is_int($float); ?> <br/><br/>
 
 
+	<?php echo "Is {$integer} float? " .  is_float($integer); ?> <br/><br/>
 
-				
-	</body>
+	<?php echo "Is {$float} float? "  .  is_float($float); ?> <br/><br/>
+
+
+</body>
 </html>
