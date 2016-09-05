0ca00076487dc7003282ed778db9553cf816e269
Stefano Lissia <stefan22@users.noreply.github.com>
2016-09-05 13:17:54 +0100

    Update associate-arrays.php

--- 02. dataTypes/associate-arrays.php
+++ 02. dataTypes/associate-arrays.php
@@ -2,32 +2,23 @@
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
-	<head>
-		<title>sandbox</title>
-	</head>
-	<body>
+<head>
+	<title>sandbox</title>
+</head>
+<body>
 		
+	<?php $assoc = array("first_name" => "Stefano", "last_name" => "Lissia"); ?>
 
-	
+	<?php echo $assoc["first_name"]; ?>
 
-		<?php $assoc = array("first_name" => "Stefano", "last_name" => "Lissia"); ?>
+	<?php echo $assoc["first_name"]; ?> <br /><br />
 
-				<?php echo $assoc["first_name"]; ?>
+	<?php echo $assoc["last_name"]; ?>  <br /> <br />
 
-				<?php echo $assoc["first_name"]; ?> <br /><br />
+	<?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?>
 
-				<?php echo $assoc["last_name"]; ?>  <br /> <br />
-
-				<?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?>
-
-				<?php $assoc["first_name"] = "Michael"; ?>
+	<?php $assoc["first_name"] = "Michael"; ?>
 
 			
-		
-
-		
-
-
-				
-	</body>
+</body>
 </html>
