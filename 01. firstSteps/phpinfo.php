85b3f5884c3183a6a7206175100d55a30fc10688
Stefano Lissia <stefan22@users.noreply.github.com>
2016-09-05 12:21:15 +0100

    Update phpinfo.php

--- 01. firstSteps/phpinfo.php
+++ 01. firstSteps/phpinfo.php
@@ -1,19 +1,10 @@
-# PHP INTRO
-
-
-    <?php
-
->//Show all information, defaults, to INFO_ALL
-
+<?php
+    //Show all information, defaults, to INFO_ALL
     phpinfo(); 
-
-
-
-    ?>
-
+?>
 
 <br />
 <br />
 
 
-*   [Back](https://github.com/stefan22/phpIntro)
+[Back](https://github.com/stefan22/phpIntro)
