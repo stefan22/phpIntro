56521aea8874577e7bac71991a92d7fbb8d8ded6
Stefan Lissia <stefan.lissia@vml.com>
2016-09-15 17:20:35 +0100

    includes layout

---
+++ 999. widget_corp/includes/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+				Copyright 20xx, Widget Corp.
+			</div> <!--  end of div footer  -->
+			
+		</div> <!--  end of div main   -->
+	</body>
+</html>
+
---
+++ 999. widget_corp/public/manage_content.php
@@ -0,0 +1,19 @@
+<?php require_once(../includes/functions.php); ?>
+<?php include("../includes/layouts/header.php");   ?>
+		
+
+<div id="main">
+			
+	<div id="navigation">
+		&nbsp;
+	</div> <!--  end of div navigation  -->
+			
+	<div id="page">
+		<h2>Manage Content</h2>
+	    
+	</div> <!--  end of div  page  -->
+
+</div>  <!--  end of div main  -->			
+			
+
+<?php include("../includes/layouts/footer.php"); ?>
