0c2420dc56c148c473c0072714faf5d344a1f40c
Eric Cartman <stefan22@users.noreply.github.com>
2016-09-22 15:50:07 +0100

    Update header.php

--- 999. widget_corp/includes/layouts/header.php
+++ 999. widget_corp/includes/layouts/header.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE>
+<html lang="en">
 	<head>
-		<title>Admin - Widget Corp</title>
-		<meta charset="utf-8">
+		<title>Admin - Widget Corp</title>		
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	    <meta name="description" content="Admin project">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-		<style type="text/css href="css/public.ss"></style>
+		<link rel="stylesheet" type="text/css" href="../../../wc/public/css/public.css" />
 	</head>
 	<body>
 		<div id="header">
